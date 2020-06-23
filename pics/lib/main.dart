a52a6e107959f9f01243f5d4dd46a19752e2fb0e
Ivan Hornung <ivanhor2003@gmail.com>
2020-06-22 19:05:03 -0700

    StatelessWidget State Design

--- pics/lib/main.dart
+++ pics/lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-  //Remember: home is the "home/main widget"
-  var app = MaterialApp(
-    home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add), //add is a + icon
-        onPressed: () {
-          print('Hi there!');
-        },
-      ),
-      appBar: AppBar(
-        title: Text('Lets see some images!'),
-      ),
-    ),
-  );
-
-  runApp(app);
+  runApp(App());
 }
---
+++ pics/lib/src/app.dart
@@ -0,0 +1,24 @@
+// Import flutter helper library
+import 'package:flutter/material.dart';
+
+// Create a class that will be our custom widget
+// This class must extend the 'StatlessWidget' base class
+class App extends StatelessWidget {
+  // Must define a 'build' method that returns
+  // the widgets that *this* widget will show
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            print('Hi there!');
+          },
+        ),
+        appBar: AppBar(
+          title: Text('Lets see some images!'),
+        ),
+      ),
+    );
+  }
+}
