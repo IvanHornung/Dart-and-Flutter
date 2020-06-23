0ba61385502e2c7ce3f715fe06fabe9cd1429103
Ivan Hornung <ivanhor2003@gmail.com>
2020-06-22 18:45:02 -0700

    Widgets: Scaffold (w title) & Button (w Icon)

--- pics/lib/main.dart
+++ pics/lib/main.dart
@@ -1,14 +1,20 @@
-// I need to import a helper library
-// from flutter to get content on the screen
 import 'package:flutter/material.dart';
 
-// Define a 'main' function to run when our app starts
 void main() {
-  // Create a new text widget to show some text on the screen
+  //Remember: home is the "home/main widget"
   var app = MaterialApp(
-    home: Text('Hi there!'),
+    home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add), //add is a + icon
+        onPressed: () {
+          print('Hi there!');
+        },
+      ),
+      appBar: AppBar(
+        title: Text('Lets see some images!'),
+      ),
+    ),
   );
 
-  // Take that widget and get it on the screen
   runApp(app);
 }
