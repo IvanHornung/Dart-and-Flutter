ca9bce56a403c4a5d2f97e1e6400789f6fb59bac
Ivan Hornung <ivanhor2003@gmail.com>
2020-06-23 20:03:47 -0700

    Images now appear when plus is pressed

--- pics/lib/src/widgets/image_list.dart
+++ pics/lib/src/widgets/image_list.dart
@@ -1,20 +1,17 @@
-
 import 'package:flutter/material.dart';
 import '../models/image_model.dart';
-
 
 //because ImageList itself is not in charge of changing variables...
 class ImageList extends StatelessWidget {
   final List<ImageModel> images;
-  
+
   ImageList(this.images);
 
   Widget build(context) {
     return ListView.builder(
-      itemCount: images.length,
-      itemBuilder: (context, int index) {
-        
-      }
-    );
+        itemCount: images.length,
+        itemBuilder: (context, int index) {
+          return Image.network(images[index].url);
+        });
   }
 }
