57af71b90ce0d384afae1913c1b806f9431b0bdb
Ivan Hornung <51543158+ivanhor2003@users.noreply.github.com>
2020-06-28 20:14:03 -0700

    Created 2nd dart project: login_stateful

---
+++ login_stateful/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+//we get access to the RunApp function;
+//how we some initial widget to appear on the device
+
+import 'src/app.dart';
+
+void main() {
+  runApp(App());
+}
