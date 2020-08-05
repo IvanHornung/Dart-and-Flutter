fa859603db41b8c1ffcc2296c7d5e5dbaf404ba3
Ivan Hornung <ivanhor2003@gmail.com>
2020-08-05 15:00:41 -0700

    Box, Stack, Center Widgets + other

--- animation/lib/src/screens/home.dart
+++ animation/lib/src/screens/home.dart
@@ -7,17 +7,85 @@ class Home extends StatefulWidget {
   HomeState createState() => HomeState();
 }
 
-class HomeState extends State<Home> {
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  //Animation class that controls a value of type double
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  //special method. everytime a HomeState is created, this is auto invoked.
+  //only special in a State-based class
+
+  initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2), //full animation length
+      vsync: this,
+      //vsync=TickerProvider: handle from outside world into our widget that
+      //gives the outside word of flutter to reach in and tell our anim to progress
+      //along and essentially render the next frame of our animation. This class is a Mixin
+    );
+
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+      CurvedAnimation(
+        parent: catController,
+        curve: Curves.easeIn, //increasing exponential change in value
+      ),
+    );
+  }
+
+  onTap() {
+    if (catController.status == AnimationStatus.completed) {
+      //@ end:100.0
+      catController.reverse(); //opposite of forward
+    } else if (catController.status == AnimationStatus.dismissed) {
+      //initial pos
+      catController.forward(); //play the animation
+    }
+  }
+
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Animation!'),
       ),
-      body: buildAnimation(),
+      body: GestureDetector(
+        child: Center(
+          //centers widgets
+          child: Stack(
+            //when you tap on the child, it detects
+            //layout widget that stacks an array of widgets on top of each other
+            children: <Widget>[
+              buildCatAnimation(), //we want the box on top of the cat, so list cat first
+              buildBox(),
+            ],
+          ),
+        ),
+        onTap: onTap,
+      ),
     );
   }
 
-  Widget buildAnimation() {
-    return Cat();
+  Widget buildCatAnimation() {
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        return Container(
+          //we will keep recreating a Container, but that is ok
+          child: child, //because it is inexpensive compared to child (Cat)
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+      },
+      child: Cat(),
+    );
+  }
+
+  //box for cat
+  Widget buildBox() {
+    return Container(
+      height: 200.0,
+      width: 200.0,
+      color: Colors.brown,
+    );
   }
 }
