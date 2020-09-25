785bbf5ac5487df505e90d3713fa46cfead3ca6c
Ivan Hornung <ivanhor2003@gmail.com>
2020-09-25 14:11:13 -0700

    Toggled off debugPaintSizeEnabled

--- animation/lib/main.dart
+++ animation/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/rendering.dart';
 import 'src/app.dart';
 
 void main() {
-  debugPaintSizeEnabled = true; //adds additional elements on the screen
+  //debugPaintSizeEnabled = true; //adds additional elements on the screen
   //of emulator to give better idea of how layout is working
 
   runApp(App());
