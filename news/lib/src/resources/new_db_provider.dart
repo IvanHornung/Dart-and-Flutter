3442abf6520a1d8225ff875591b6e10077490bcb
Ivan Hornung <ivanhor2003@gmail.com>
2020-09-25 20:41:37 -0700

    Skeleton to create table with SQLite

--- news/lib/src/resources/new_db_provider.dart
+++ news/lib/src/resources/new_db_provider.dart
@@ -9,5 +9,25 @@ class NewsDbProvider {
   Database db;
 
   //cannot assign async to a Constructor, so...
-  init() async {}
+  //init tries to create a database or attempt to reopen it
+  init() async {
+    //for mobile devices, returns reference to a little folder where we can safely store some data
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    //documentsDirectory is a folder reference. $.path speaks for itself.
+    final path = join(documentsDirectory.path,
+        "items.db"); //join the path + the word "items.db" together
+    db = await openDatabase(
+      path, //create if not exist, or just use path
+      version: 1, //internal record for me
+      onCreate: (Database newDb, int version) {
+        //only called the very first time our user starts our applications
+        newDb.execute("""
+          CREATE TABLE Items
+          (
+            
+          )
+        """);
+      },
+    );
+  }
 }
