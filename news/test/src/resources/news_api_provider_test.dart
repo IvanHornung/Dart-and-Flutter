bfcc9eaf326fd3e815c2cfa3ebedd0bfea0a5c8b
Ivan Hornung <ivanhor2003@gmail.com>
2020-09-25 20:04:18 -0700

    Sample test code

--- news/test/src/resources/news_api_provider_test.dart
+++ news/test/src/resources/news_api_provider_test.dart
@@ -3,3 +3,12 @@ import 'dart:convert';
 import 'package:test/test.dart';
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
+
+void main() {
+  test('FetchTopIds returns a list of ids', () {
+    //set up of test case
+    final sum = 1 + 3;
+    //expectation
+    expect(sum, 4);
+  });
+}
